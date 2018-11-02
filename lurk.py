92064d900119ca1abfe63ce60280b6e7a1603054
lil bo weep <davianasales@outlook.com>
2018-11-01 21:23:07 -0300

    Limpeza de código e remoção dos testes de port que não funcionavam

--- lurk.py
+++ lurk.py
@@ -24,18 +24,18 @@ class cores:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-'''parser = argparse.ArgumentParser(description='Gera endereços pseudoaleatórios de IPs e testa todos.')
-parser.add_argument('tentativas', metavar='t', type=int, nargs='+', help='um numero inteiro de tentativas')
-
-args = parser.parse_args()
-print args.accumulate(args.tentativas)'''
+''' > Classe para segurar as respostas '''
+class saida:
+	def ipResultado(res):
+		if(res >= 0):
+			return cores.HEADER + cores.WARNING + "     [+] " + cores.OKGREEN + "O IP respondeu."
+		else:
+			return cores.HEADER + cores.WARNING + "     [-] " + cores.FAIL + "O IP não respondeu."
 
 DECLARE_PATH = 'lurk_dump.txt'
 
-
 os.system('cls' if os.name == 'nt' else 'clear')
 print("***********************************")
-print("+++ Lurk 0.3a // We are listening.")
 print("+++ Iniciado em " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 print("***********************************")
 print("");
@@ -46,21 +46,10 @@ while True:
 	print(cores.HEADER + cores.WARNING + "[+] " + cores.OKGREEN + "Atualmente testando " + cores.OKBLUE + ip  + cores.OKGREEN + " ↴")
 	try:
 		socket.gethostbyaddr(ip)
-		print(cores.HEADER + cores.WARNING + "     [+] " + cores.OKGREEN + "O IP respondeu.")
+		print(saida.ipResultado(0))
 		tester = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		tester.settimeout(3)
 		test22 = tester.connect_ex((ip, 22))
-		if test22 == 0:
-			print(cores.HEADER + cores.WARNING + "     [+] " + cores.OKGREEN + "Port 22: aberto.")
-		else:
-			print(cores.HEADER + cores.WARNING + "     [+] " + cores.FAIL + "Port 22: fechado.")
-		testersql = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		testersql.settimeout(3)
-		tester3306 = testersql.connect_ex((ip, 3306))
-		if tester3306 == 0:
-			print(cores.HEADER + cores.WARNING + "     [+] " + cores.OKGREEN + "Port 3306: aberto.")
-		else:
-			print(cores.HEADER + cores.WARNING + "     [+] " + cores.FAIL + "Port 3306: fechado.")
 		print("")
 		if os.path.exists(DECLARE_PATH):
 			with open(DECLARE_PATH, 'a') as arq:
@@ -71,8 +60,6 @@ while True:
 			with open(DECLARE_PATH, 'a') as arq:
 				arq.write(ip + '\n')
 
-
-
 	except socket.herror:
-		print(cores.HEADER + cores.WARNING + "     [-] " + cores.FAIL + "O IP não respondeu.")
+		print(saida.ipResultado(-1))
 		print("");
